46cdb1d2f741928ce6483778289a816e308a26bf
Admin Login <adminlogin@Admins-MacBook-Pro-2.local>
2023-11-08 01:57:50 +0530

    test home

--- src/components/CustomerReviewCard.jsx
+++ src/components/CustomerReviewCard.jsx
@@ -1,16 +1,19 @@
 import { Image } from 'antd';
 import React from 'react';
 import { AiOutlineCar } from 'react-icons/ai';
+import { useLocation } from 'react-router';
 
-export const CustomerReviewCard = () => {
+export const CustomerReviewCard = ({item}) => {
+  const location = useLocation();
+  const currentPath = location.pathname;
   return (
     <div className="card card-compact bg-base-100 shadow-xl rounded-lg h-auto w-auto bg-cover bg-[url('https://demo-egenslab.b-cdn.net/html/drivco/preview/assets/img/inner-page/inner-bg.png')]">
       <div className="m-4">
         <figure className="mb-4">
           <Image
-            src="https://media.zigcdn.com/media/model/2021/May/v8_360x240.jpg"
-            className="rounded-lg borded-solid border-2"
-            alt="Shoes"
+            src={item?.image?.url} width='100%'
+            className="rounded-lg borded-solid border-2 !h-44 !object-cover"
+            alt="review"
           />
         </figure>
         <div className="card-body !p-0 flex flex-col gap-2">
@@ -18,13 +21,13 @@ export const CustomerReviewCard = () => {
             <div className="border border-blue-500 rounded-full flex items-center justify-center">
               <AiOutlineCar className=" text-blue-500" />
             </div>
-            <h1 className="font-medium">model name here</h1>
+            <h1 className="font-medium"><span className='capitalize'>{item?.brand}</span> - <span className='uppercase'>{item?.model}</span></h1>
           </div>
-          <div>
-            Customer review Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          <div className={currentPath==='/' && 'truncate'}>
+            {item?.review}
           </div>
           <div className="flex card-actions justify-end p-1">
-            __customer name
+            __{item?.customerName}
           </div>
         </div>
       </div>
