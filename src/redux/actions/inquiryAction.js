7749f95f918fc61439b82c94265978f784ec5e93
KirtimaanVishwakarma <vishwakarma.kirtimaan@gmail.com>
2023-11-26 15:57:32 +0530

    feat/postinquiry

--- src/redux/actions/inquiryAction.js
+++ src/redux/actions/inquiryAction.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 import { INQUIRY } from "../../utils/apiConstant";
-import { data } from "autoprefixer";
 export const addInquiry = (myForm) => async (dispatch) => {
   try {
     dispatch({ type: "addInquiryRequest" });
     let { data } = await axios.post(INQUIRY, myForm, {
       headers: {
-        "Content-Type": "multipart/form-data",
+        "Content-Type": "application/json",
       },
       withCredentials: true,
     });
@@ -14,7 +13,6 @@ export const addInquiry = (myForm) => async (dispatch) => {
   } catch (err) {
     dispatch({ type: "addInquiryError", payload: err.response.data.message });
   }
-  console.log(data);
 };
 
 export const getInquiry = () => async (dispatch) => {
